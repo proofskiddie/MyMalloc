34bcec9d983a3a6d3cd5fd0908340402c6411f6c
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-01-27 16:30:45 -0500

    Commit

--- test6.c
+++ test6.c
@@ -17,7 +17,7 @@ void *allocationThread(void *none){
 int main(){
 
     printf("\n---- Running test6 ---\n");
-/*    int numThreads = 5;
+    int numThreads = 5;
     malloc(20);
 
     pthread_t threads[numThreads];
@@ -32,5 +32,5 @@ int main(){
 
     print_list();
     exit(0);
-*/
+
 }
